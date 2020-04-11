b91044ba58180c693c50500f6c1f25d2e1686890
Soojung Kwak <gsj2464@gmail.com>
2020-04-11 14:25:21 -0400

    added fontawesome

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Menu } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm, faThumbsUp, faVideo } from "@fortawesome/free-solid-svg-icons";
 
 function LeftMenu(props) {
   return (
@@ -9,15 +11,15 @@ function LeftMenu(props) {
       </Menu.Item>
     
       <Menu.Item key="subscription">
-        <a href="/subscription">Subscription</a>
+        <a href="/subscription"><FontAwesomeIcon icon={faVideo} size="lg" /> Subscription</a>
       </Menu.Item>
 
       <Menu.Item key="movie">
-        <a href="/movie">Movie</a>
+        <a href="/movie"><FontAwesomeIcon icon={faFilm} size="lg" /></a>
       </Menu.Item>
 
       <Menu.Item key="favorite">
-        <a href="/movie/favorite/list">Favorite</a>
+        <a href="/movie/favorite/list"><FontAwesomeIcon icon={faThumbsUp} size="lg" /> Favorite</a>
       </Menu.Item>
     </Menu>
   )
